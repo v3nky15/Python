46bfda5879010bba83bfab28ef8bf3240c21b2a1
v3nky15 <141437519+v3nky15@users.noreply.github.com>
2024-05-09 23:25:41 +0000

    added split program

---
+++ day-1/datatype &examples/split.py
@@ -0,0 +1,4 @@
+#write a program for split the name or string
+
+arn = "arn:aws:iam::123456789012:user/johndoe"
+print(arn.split("/"))
---
+++ day-1/datatype &examples/test.py
@@ -0,0 +1,4 @@
+#write a program for split the name or string
+
+arn  = input("enter the string with / :")
+print(arn.split("/"))
